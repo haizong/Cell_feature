83edfcbcf6a2497920e5477b8392a4c99a526c47
haizong <haizong@indiana.edu>
2017-03-09 14:27:25 -0500

    add notes

--- FA_identify.m
+++ FA_identify.m
@@ -19,7 +19,7 @@ tic
 % dir_name = '20170109_R1. MCAK2'; 
 % dir_name = '20170109_R2. Neg2';
 % dir_name = '20170109_R2. MCAK2';
-% dir_name = '20170109_R3. Neg2';
+dir_name = '20170109_R3. Neg2';
 % dir_name = '20170109_R3. MCAK2';
 fontsize = 12;
 size_scale = 0.5; % Image is too. Convert to 1024 x 768 image to display.
@@ -42,7 +42,7 @@ cell_data = focalAdhesionIdentify (cell_data, dir_name, int_thresh_FA, ...
 %% Manually remove joint cells
 % '20170109_R1. Neg2' #5, #20 cells too close to be separated. 
 %% Remove unwanted fields and Save. Too big to save all info. 
-fields = {'ori_img', 'Red_gray', 'labeledImage', 'actin_rps'};
+fields = {'ori_img', 'Red_gray', 'labeledImage'};
 cell_data_rps = rmfield( cell_data, fields);  
 save( ['FA_rps_', dir_name([1:11,14:end])],... % rm space in filename
     'cell_data_rps', 'dir_name', 'size_thresh_actin_low', 'size_thresh_actin_up',...
